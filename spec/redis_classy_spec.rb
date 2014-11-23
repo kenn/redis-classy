3e330d2dc8cf063cf81d409ef9ec361f13cf375e
Kenn Ejima <kenn.ejima@gmail.com>
2014-11-22 17:48:36 -0800

    RSpec 3

--- spec/redis_classy_spec.rb
+++ spec/redis_classy_spec.rb
@@ -23,31 +23,31 @@ describe Redis::Classy do
 
   it 'prepends class name to the key' do
     Something.set('foo', 'bar')
-    Something.keys.should == ['foo']
-    Redis::Classy.keys.should include 'Something:foo'
+    expect(Something.keys).to eq(['foo'])
+    expect(Redis::Classy.keys).to include 'Something:foo'
 
     Another.set('foo', 'bar')
-    Another.keys.should == ['foo']
-    Redis::Classy.keys.should include 'Another:foo'
+    expect(Another.keys).to eq(['foo'])
+    expect(Redis::Classy.keys).to include 'Another:foo'
 
     Deep::Klass.set('foo', 'bar')
-    Deep::Klass.keys.should == ['foo']
-    Redis::Classy.keys.should include 'Deep::Klass:foo'
+    expect(Deep::Klass.keys).to eq(['foo'])
+    expect(Redis::Classy.keys).to include 'Deep::Klass:foo'
   end
 
   it 'delegates to class methods' do
-    Something.get('foo').should == nil
+    expect(Something.get('foo')).to eq(nil)
     Something.set('foo', 'bar')
-    Something.get('foo').should == 'bar'
+    expect(Something.get('foo')).to eq('bar')
   end
 
   it 'delegates instance methods with the key binding' do
     something = Something.new('foo')
 
-    something.get.should == nil
+    expect(something.get).to eq(nil)
     something.set('bar')
-    something.get.should == 'bar'
-    Something.get('foo').should == 'bar'
+    expect(something.get).to eq('bar')
+    expect(Something.get('foo')).to eq('bar')
   end
 
   it 'handles multi block' do
@@ -59,7 +59,7 @@ describe Redis::Classy do
       something.sadd 3
     end
 
-    something.smembers.should == ['1','2','3']
+    expect(something.smembers).to eq(['1','2','3'])
   end
 
   it 'handles method_missing' do
@@ -81,7 +81,7 @@ describe Redis::Classy do
 
     # This would have returned "foo" instead of nil, unless we explicitly defined
     # class methods from Redis::Namespace::COMMANDS
-    Something.get('foo').should == nil
+    expect(Something.get('foo')).to eq(nil)
 
     class << Object
       remove_method :get
