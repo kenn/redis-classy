860fbde610d68b953209973e11ff329711cfdb3e
Kenn Ejima <kenn.ejima@gmail.com>
2016-10-06 13:05:29 -0400

    Somehow test results are different on Mac vs Travis

--- spec/redis_classy_spec.rb
+++ spec/redis_classy_spec.rb
@@ -116,7 +116,7 @@ describe RedisClassy do
     Stats.average.set ages.inject(:+)/ages.size
     expect(Stats.median.get).to eq('24')
     expect(Stats.average.get).to eq('25')
-    expect(Stats.keys.map(&:to_sym)).to eq([:median, :average])
+    expect([:average, :median] - Stats.keys.map(&:to_sym)).to eq([])
     expect(Stats.singletons_keys).to eq([:median, :average])
   end
 
